87ffb893a2e7d3756ae178775c38fd94ccd30c93
jvesguerra <jvesguerra@up.edu.ph>
2023-05-18 01:21:21 +0800

    first commit

---
+++ src/machine.java
@@ -0,0 +1,90 @@
+import java.util.LinkedHashMap;
+import java.util.Scanner;
+
+public class machine {
+
+	public static void main(String[] args) {
+
+
+		Scanner sc = new Scanner(System.in);
+		int i, j, x, temp_value, limit, index;
+		final int FIVE = 5;
+		final int TEN = 10;
+		LinkedHashMap<Integer, Integer> amounts = new LinkedHashMap<>(); // will be dispensed
+		LinkedHashMap<Integer, Integer> machine = new LinkedHashMap<>(); // machine money
+		int dispensing_amount, total_amount = 0;
+		int[] bills = {100, 50, 20, 5, 1};
+
+		// create maps
+		for(i = 0; i < FIVE; i++){
+			amounts.put(bills[i], 0);
+			machine.put(bills[i], 0);
+		}
+
+		// save user inputs to amounts array
+		System.out.print("Enter amounts: \n" );
+        for (Integer key : machine.keySet()) {
+        	machine.merge(key,0,(k,v)->sc.nextInt());
+        }
+
+        for (Integer key : machine.keySet()) {
+        	System.out.println(key + " = " + machine.get(key));
+        	total_amount = total_amount + (key * machine.get(key));
+        }
+
+        System.out.println("\nTotal amount: " + total_amount);
+
+        System.out.println("\nDispensing Amount: ");
+        dispensing_amount = sc.nextInt();
+
+       // check if machine has enough money
+        if(total_amount < dispensing_amount){
+        	System.out.println("Not enough money in the machine");
+        }else{
+            String str_amount = String.valueOf(dispensing_amount);	// gets length of dispensing amount
+            String[] digits = str_amount.split("");
+
+            int length = digits.length;
+            int multiplier = 1;
+            int[] values = new int[length];
+
+            // 125 --> 100, 20, 5
+            for(i = length-1; i >= 0; i--){
+            	values[i] = Integer.parseInt(digits[i]) * multiplier;
+            	multiplier = multiplier * TEN;
+            }
+
+            int goal = 0;
+            for(i = 0; i < length; i++){
+            	temp_value = 0;
+            	//System.out.println(values[i]);
+                for (Integer key : amounts.keySet()) {
+            		for(j = 0; j < machine.get(key); j++){
+            			index = j + 1;
+            			temp_value = key * index;
+            			if(temp_value == values[i]){
+            				if(goal != dispensing_amount){
+            					amounts.put(key, index);
+                				goal = goal + temp_value;
+            				}
+            				temp_value = 0;
+
+            				if(i != length-1) i = i + 1;
+            			}
+            		}
+
+                }
+
+            }
+            for (Integer key : amounts.keySet()) {
+            	if(amounts.get(key) != 0){
+            		System.out.print("Amount: "+ amounts.get(key) + " P" + key + " ");
+            	}
+
+            }
+        }
+
+
+	}
+
+}
